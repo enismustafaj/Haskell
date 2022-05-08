8907d523eee5217bb8a5e72bde5d148468c071c9
Enis Mustafaj <40141091+enismustafaj@users.noreply.github.com>
2022-05-08 10:40:42 +0200

    Update leap_year.hs

--- leap_year.hs
+++ leap_year.hs
@@ -1,21 +1,31 @@
+{--
 Enis Mustafaj
 e.mustafaj@jacobs-university.de
+--}
 
+
+{--
 Problem 2.3
-a)
+--}
+
+{-- a) --}
+
 isLeapYear :: Int -> [Char]
 isLeapYear x =if x`mod`400==0 || (x `mod` 4 ==0 && x `mod` 100 /=0 )  then "Leap Year" else "Not Leap Year"
-b)
+{-- b) --}
+
 isLeapYear' y  | y `mod` 400 == 0 = "Leap year" | y `mod` 100 == 0 = "Not Leap Year"| y `mod` 4==0 = "Leap Year"| otherwise = "Not Leap Year"
 
 
-
+{--
 Problem 2.4
 a)
+--}
+
 rotate :: Int -> [a] -> [a]
 rotate 0 x = x
 rotate n x = rotate (n-1) (tail x ++ [head x])
 
-b)
+{-- b) --}
 circle :: [a] -> [[a]]
 circle y = [rotate x y| x<- [0..(length y - 1)]]
